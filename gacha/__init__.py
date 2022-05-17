7ba0b3f9e2676b1983972eec51dcce30d9fc8da0
黛笙笙 <30380477+DaiShengSheng@users.noreply.github.com>
2022-05-17 22:03:23 +0800

    添加卡池抽卡次数限制

--- gacha/__init__.py
+++ gacha/__init__.py
@@ -1,10 +1,17 @@
 from hoshino import Service, priv, MessageSegment
 from hoshino.typing import HoshinoBot,CQEvent
+from hoshino.util import DailyNumberLimiter
 from .gacha import *
-sv = Service("雀魂抽卡")
 
+sv = Service("雀魂抽卡")
+daily_limiter_10 = DailyNumberLimiter(10)
 
 @sv.on_fullmatch('雀魂十连')
 async def majsoul_gacha(bot, ev: CQEvent):
+    userid = ev['user_id']
+    if not daily_limiter_10.check(userid):
+        await bot.send(ev, '今天已经抽了很多次啦，明天再来吧~')
+        return
     img = run_gacha()
+    daily_limiter_10.increase(userid)
     await bot.send(ev, mes + MessageSegment.image(img), at_sender=True)
