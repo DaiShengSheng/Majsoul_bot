65f7508b0855a54bcd9b3d682524fdbf76037e3f
YukikazeKagero <102728875+YukikazeKagero@users.noreply.github.com>
2022-07-11 15:45:09 +0800

    Update __init__.py
    修复bug：ERROR: cannot import name 'MessageSegment' from 'hoshino'

--- mahjong_handle/__init__.py
+++ mahjong_handle/__init__.py
@@ -1,4 +1,5 @@
-from hoshino import Service, MessageSegment, priv
+from hoshino import Service, priv
+from hoshino.typing import MessageSegment
 from .handler import HandGuess
 from .utils import get_path
 
